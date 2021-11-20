8c6d4e08a781f94fbd0175d5e16f4b78ddb234af
Orapan Talalaksana <orapan-tal63@tbs.tu.ac.th>
2021-11-20 12:30:16 +0700

    งานทำในคาบ Your App - เลือก สักส่วนในแอพกลุ่ม

--- team_app/lib/main.dart
+++ team_app/lib/main.dart
@@ -1,126 +1,105 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:icovid/constants/color_constant.dart';
+import 'package:icovid/controllers/booking_controller.dart';
+import 'package:icovid/models/booking_model.dart';
+import 'package:icovid/pages/login_page.dart';
+import 'package:icovid/services/booking_service.dart';
 import 'package:provider/provider.dart';
-import 'package:team_app/pages/hospital_scform_page.dart';
-import 'package:team_app/pages/hospital_show_page.dart';
 
-import 'model/model_hostpital_info_form.dart';
-import 'pages/hbooking_list_page.dart';
-import 'pages/hostpital_info_page.dart';
+import 'constants/font_sonstant.dart';
+import 'models/booking_list_model.dart';
+import 'models/hospital_model.dart';
+import 'models/patient_form_model.dart';
+import 'models/patient_form_model_hospitel.dart';
+import 'models/profile_model.dart';
+import 'models/user_provider.dart';
+import 'pages/booking_step1_page.dart';
+import 'pages/bottom_nav_page.dart';
+import 'pages/hospital_home_page.dart';
+import 'pages/list_user_page.dart';
+import 'pages/patient_list_page.dart';
+import 'pages/qr_scan_page.dart';
+import 'pages/signup_page.dart';
 import 'pages/ubooking_list_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // var services = FirebaseServices();
+  // var controller = BookingController(services);
+
   runApp(
     MultiProvider(
-      providers:[
+      providers: [
         ChangeNotifierProvider(
-          create: (context) => HospitalinfoFormModel(),
+          create: (context) => BookingModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProfileModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => BookingListModel(),
+        ),
+        ChangeNotifierProvider(
+          //ของกิ๊ฟ
+          create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          //ของโบนัส
+          create: (context) => HospitalFormModel(),
+        ),
+        ChangeNotifierProvider(
+          //ของแตง
+          create: (context) => PatientFormModel(),
+        ),
+        ChangeNotifierProvider(
+          //ของแตง
+          create: (context) => PatientFormModelHospitel(),
         ),
       ],
-      child: MyApp(),
+      child: BookingApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  
+class BookingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'I-Covid App',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-    initialRoute: '/5',
-     routes : <String, WidgetBuilder> {
-       '/1':(context)=> UBookingListScreen (),
-       '/2':(context)=> HostpitalInfoScreen (),
-       '/3':(context)=> HBookingListScreen (),
-       '/4':(context)=> HostpitalScreen (),
-       '/5':(context)=> Hospitalshow (),
-     }
+      home: BottomNavScreen(),
+      routes: <String, WidgetBuilder>{
+        '/1': (context) => SignUpScreen(),
+        '/2': (context) => LogInScreen(),
+        '/3': (context) => BookingStep1Screen(),
+        '/4': (context) => HospitalHomeScreen(),
+        '/5': (context) => PatientListPage(),
+        '/6': (context) => ListUser(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() { 
-      _counter++;
-    });
-  }
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            onPressed: (){
-              Navigator.push(context,
-              MaterialPageRoute(
-                builder: (context)=> UBookingListScreen()
-                ) 
-              );
-            },
-            icon: Icon(Icons.list_alt_rounded),
-          ),
-          IconButton(
-            onPressed: (){
-              Navigator.push(context,
-              MaterialPageRoute(
-                builder: (context)=> HBookingListScreen()
-                ) 
-              );
-            },
-            icon: Icon(Icons.local_hospital),
-          ),
-          IconButton(
-            onPressed: (){
-              Navigator.push(context,
-              MaterialPageRoute(
-                builder: (context)=> HostpitalInfoScreen()
-                ) 
-              );
-            },
-            icon: Icon(Icons.add),
-          )
-        ],
+    return MaterialApp(
+      title: 'i-Covid',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: fontRegular,
       ),
-      body: Center(
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      home: AnimatedSplashScreen(
+        splash: Image.asset('assets/images/launcher_icon.png'),
+        duration: 2500,
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: iWhiteColor,
+        nextScreen: LogInScreen(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
     );
   }
 }
